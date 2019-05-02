b38a089c92bc8069cfc79372d01200ff23877e13
baddecisionsalex <alex.ameen.tx@gmail.com>
2019-05-02 18:02:54 -0500

    adding python3 files

---
+++ libigcc3/source_code.py
@@ -0,0 +1,46 @@
+
+# igcc - a read-eval-print loop for C/C++ programmers
+#
+# Copyright (C) 2009 Andy Balaam
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+file_boilerplate = """#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
+
+$user_includes
+
+$user_functions
+
+int
+main( void )
+{
+	$user_commands
+
+	return 0;
+}
+"""
+
+
+def get_full_source( runner ):
+	return ( file_boilerplate
+		.replace( "$user_functions", runner.get_user_functions_string() )
+		.replace( "$user_commands", runner.get_user_commands_string() )
+		.replace( "$user_includes", runner.get_user_includes_string() )
+		)
