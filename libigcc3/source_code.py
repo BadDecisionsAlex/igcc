f6b96af14b242fe1f44738ffba07eafb8b4d56c4
baddecisionsalex <alex.ameen.tx@gmail.com>
2019-05-02 20:56:59 -0500

    code is automatically formatted

--- libigcc3/source_code.py
+++ libigcc3/source_code.py
@@ -18,28 +18,41 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
+import subprocess
+
 file_boilerplate = ("""\
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 $user_includes
-
 $user_functions
-
 int
 main( void )
 {
     $user_commands
-
     return 0;
 }
 """ )
 
 
+def format_code( source_code ):
+    format_process = subprocess.Popen(
+      ['uncrustify', '-l', 'c', '-q', '-c', '/home/camus/.uncrustify.cfg'],
+      stdin = subprocess.PIPE, stdout = subprocess.PIPE )
+
+    stdoutdata, stderrdata = format_process.communicate(
+      source_code.encode( 'utf-8') )
+
+    if stderrdata is not None:
+        return source_code
+    else:
+        return stdoutdata.decode( 'utf-8' )
+
+
 def get_full_source( runner ):
-        return ( file_boilerplate
-                .replace( "$user_functions", runner.get_user_functions_string() )
-                .replace( "$user_commands", runner.get_user_commands_string() )
-                .replace( "$user_includes", runner.get_user_includes_string() )
-                )
+    with_replacements = ( file_boilerplate
+      .replace( "$user_functions", runner.get_user_functions_string() )
+      .replace( "$user_commands", runner.get_user_commands_string() )
+      .replace( "$user_includes", runner.get_user_includes_string() )
+      )
+    return format_code( with_replacements )
